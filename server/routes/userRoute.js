546ac010135f0a7cec7bb21cf8e4b02917930ea0
yishak <yishak.110402@gmail.com>
2025-04-20 23:19:13 +0300

    Added User search and edit

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const { createUser, logIn, editUserDetails } = require("../controllers/userControllers")
+const { createUser, logIn, editUserDetails, fetchUser } = require("../controllers/userControllers")
 const router = express.Router()
 
 router.route("/").post(createUser).put(logIn)
-router.route("/:id").patch(editUserDetails)
+router.route("/:id").patch(editUserDetails).get(fetchUser)
 
 module.exports = router
